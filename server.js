14979eb36012a867448041371097ef125002b458
adityapandey <89adityapandey@gmail.com>
2026-09-18 14:54:16 +0530

    basic express ecommerce

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+const cartRoutes = require('./routes/cartRoutes');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/users', userRoutes);
+app.use('/products', productRoutes);
+app.use('/cart', cartRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
